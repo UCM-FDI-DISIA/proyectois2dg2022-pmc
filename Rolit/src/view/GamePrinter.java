2d427eb51148417c18581f1d2d039adcd646c94f
unknown <marram18@ucm.es>
2022-02-15 16:41:41 +0100

    gamePrinter + javaUtils

---
+++ Rolit/src/view/GamePrinter.java
@@ -0,0 +1,31 @@
+package view;
+
+import utils.StringUtils;
+
+public class GamePrinter {
+	
+	private static final String SPACE = " ";
+	private static final int MARGIN_SIZE = 2; 
+	private static final int GAP_SIZE = 3;
+	private Game game;
+	
+	public GamePrinter(Game game) {
+		this.game = game;
+	}
+	
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		
+		// Paint game
+		for (int x = 0; x < game.getBoardSize(); x++) {
+			str.append(MARGIN_SIZE);
+			for (int y = 0; y < game.getBoardSize(); y++) {
+				str.append(StringUtils.centre(game.positionToString(x, y), GAP_SIZE)).append(SPACE);
+			}
+			str.append(StringUtils.LINE_SEPARATOR);
+			
+		}
+		
+		return str.toString();
+	}
+}
