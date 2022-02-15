c5247b264856a947ac69431ce6a32d2dc116cbdd
segarc21 <segarc21@ucm.es>
2022-02-15 21:41:22 +0100

    Atributo name de Player
    Añadido atributo name en Player.

--- Rolit/src/logic/Player.java
+++ Rolit/src/logic/Player.java
@@ -3,10 +3,12 @@ package logic;
 public class Player {
 	private Color color;
 	private int score;
+	private String name;
 	
-	public Player(Color c) {
+	public Player(Color c, String name) {
 		this.color = c;
 		this.score = 0;
+		this.name = name;
 	}
 	
 	public Color getColor() {
@@ -20,4 +22,5 @@ public class Player {
 	public void addScore(int score) {
 		this.score += score;
 	}
+	
 }
