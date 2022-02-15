73433c1cb7bbf1bf50564c594dba9e03ec88979a
JuanDiBar <juandbar@ucm.es>
2022-02-15 16:51:53 +0100

    Merge correcto

---
+++ Rolit/src/Rolit/Controller.java
@@ -0,0 +1,26 @@
+package Rolit;
+
+import java.util.Scanner;
+
+public class Controller {
+	private Scanner input;
+	private Game game;
+	
+	public Controller(Game game, ) {
+		
+	}
+	
+	private void parse(String command) {
+		
+	}
+	
+	public void run() {
+		while(!game.isFinish()) {
+			String command;
+			this.parse(command);
+			input.get()
+			game.play(x,y);
+			
+		}
+	}
+}
