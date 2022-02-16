88426acc728b8c298ce024468eb2dd66ca68092a
Masaleo <lmacia01@ucm.es>
2022-02-16 14:02:47 +0100

    Player es comparable y Cube serializable

--- Rolit/src/logic/Player.java
+++ Rolit/src/logic/Player.java
@@ -1,6 +1,6 @@
 package logic;
 
-public class Player {
+public class Player implements Comparable {
 	private Color color;
 	private int score;
 	private String name;
@@ -21,6 +21,18 @@ public class Player {
 	
 	public void addScore(int score) {
 		this.score += score;
+	}
+
+	@Override
+	public int compareTo(Object o) { //FIXME SI FALLA EL ORDEN ES AQUÍ
+		Player p = (Player) o;
+		if(score < p.getScore()) {
+			return -1;
+		}
+		else if(score == p.getScore()) {
+			return 0;
+		}
+			return 1;
 	}
 	
 }
