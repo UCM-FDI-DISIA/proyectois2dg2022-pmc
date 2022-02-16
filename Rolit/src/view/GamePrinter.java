8db5132bd2c423c6c3d021a6509198a3860981fc
unknown <marram18@ucm.es>
2022-02-16 14:27:00 +0100

    Añadir showRanking

--- Rolit/src/view/GamePrinter.java
+++ Rolit/src/view/GamePrinter.java
@@ -1,5 +1,10 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import logic.Game;
+import logic.Player;
 import utils.StringUtils;
 
 public class GamePrinter {
@@ -7,6 +12,11 @@ public class GamePrinter {
 	private static final String SPACE = " ";
 	private static final int MARGIN_SIZE = 2; 
 	private static final int GAP_SIZE = 3;
+	private static final String RANKING = "RANKING DEL ROLIT";
+	private static final String MSG_POS = "En la posicion numero ";
+	private static final String SEPARATOR = " :";
+	private static final String MSG_REY = "QUIEN SERA EL REYYYYYY?????? :)";
+	private static final String MSG_GOOD_LUCK = "suerte para la siguiente :)";
 	private Game game;
 	
 	public GamePrinter(Game game) {
@@ -15,7 +25,7 @@ public class GamePrinter {
 	
 	public String toString() {
 		StringBuilder str = new StringBuilder();
-		
+		str.append(StringUtils.LINE_SEPARATOR);
 		// Paint game
 		for (int x = 0; x < game.getBoardSize(); x++) {
 			str.append(MARGIN_SIZE);
@@ -27,5 +37,21 @@ public class GamePrinter {
 		}
 		
 		return str.toString();
+	}
+	
+	public static String showRanking() {
+		List<Player> players = game.getPlayers();
+		Collections.sort(players);
+		StringBuilder str = new StringBuilder(RANKING);
+		
+		str.append(StringUtils.LINE_SEPARATOR).append(MSG_REY).append(StringUtils.LINE_SEPARATOR);
+		
+		for (int i = 0; i < players.size(); ++i) {
+			str.append(MSG_POS).append(i).append(SEPARATOR).append(players.get(i).getName()).append(StringUtils.LINE_SEPARATOR);
+		}
+		
+		str.append(MSG_GOOD_LUCK).append(StringUtils.LINE_SEPARATOR);
+		
+		return str.toString(); 
 	}
 }
