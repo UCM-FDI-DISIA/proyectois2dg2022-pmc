e859732abfa6f92eaa4b3802544e678f1fff57b6
unknown <marram18@ucm.es>
2022-04-03 20:16:41 +0200

    arreglado error en junits

--- Rolit/tests/Builders/GameTeamsBuilderTest.java
+++ Rolit/tests/Builders/GameTeamsBuilderTest.java
@@ -10,14 +10,14 @@ import logic.Game;
 public class GameTeamsBuilderTest {
 	@Test
 	void test_1() {
-		String inputJSon = "{\"teams\":[{\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"name\":\"equipo1\"},{\"players\":[{\"color\":\"R\",\"name\":\"dani\"}],\"name\":\"equipo2\"}],\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"turn\":\"BROWN\",\"type\":\"GameTeams\",\"board\":{\"shape\":\"SM\",\"cubes\":[{\"color\":\"W\",\"pos\":[3,4]},{\"color\":\"Y\",\"pos\":[3,5]},{\"color\":\"Y\",\"pos\":[3,6]}]}}";
+		String inputJSon = "{\"teams\":[{\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"name\":\"equipo1\"},{\"players\":[{\"color\":\"R\",\"name\":\"dani\"}],\"name\":\"equipo2\"}],\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"turn\":\"W\",\"type\":\"GameTeams\",\"board\":{\"shape\":\"SM\",\"cubes\":[{\"color\":\"W\",\"pos\":[3,4]},{\"color\":\"Y\",\"pos\":[3,5]},{\"color\":\"Y\",\"pos\":[3,6]}]}}";
 		Game game = GameTeamsBuilder.createGame(new JSONObject(inputJSon));
 		assertTrue(new JSONObject(inputJSon).similar(game.report()));
 	}
 	
 	@Test
 	void test_2() {
-		String inputJson = "{\"teams\":[{\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"name\":\"equipo1\"},{\"players\":[{\"color\":\"R\",\"name\":\"dani\"}],\"name\":\"equipo2\"}],\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"turn\":\"BROWN\",\"type\":\"GameTeams\",\"board\":{\"shape\":\"SM\",\"cubes\":[{\"color\":\"W\",\"pos\":[3,4]},{\"color\":\"Y\",\"sidjgpie\":[3,5]},{\"color\":\"Y\",\"pos\":[3,6]}]}}";
+		String inputJson = "{\"teams\":[{\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"name\":\"equipo1\"},{\"players\":[{\"color\":\"R\",\"name\":\"dani\"}],\"name\":\"equipo2\"}],\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"turn\":\"W\",\"type\":\"GameTeams\",\"board\":{\"shape\":\"SM\",\"cubes\":[{\"color\":\"W\",\"pos\":[3,4]},{\"color\":\"Y\",\"sidjgpie\":[3,5]},{\"color\":\"Y\",\"pos\":[3,6]}]}}";
 		assertThrows(Exception.class, () -> GameTeamsBuilder.createGame(new JSONObject(inputJson)));
 	}
 	
@@ -36,14 +36,14 @@ public class GameTeamsBuilderTest {
 	@Test
 	void test_5() {
 		// if type is not GameClassic it returns null
-		String inputJson = "{\"teams\":[{\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"name\":\"equipo1\"},{\"players\":[{\"color\":\"R\",\"name\":\"dani\"}],\"name\":\"equipo2\"}],\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"webier\":\"BROWN\",\"type\":\"GameTeams\",\"board\":{\"shape\":\"SM\",\"cubes\":[{\"color\":\"W\",\"pos\":[3,4]},{\"color\":\"Y\",\"pos\":[3,5]},{\"color\":\"Y\",\"pos\":[3,6]}]}}";
+		String inputJson = "{\"teams\":[{\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"name\":\"equipo1\"},{\"players\":[{\"color\":\"R\",\"name\":\"dani\"}],\"name\":\"equipo2\"}],\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"webier\":\"W\",\"type\":\"GameTeams\",\"board\":{\"shape\":\"SM\",\"cubes\":[{\"color\":\"W\",\"pos\":[3,4]},{\"color\":\"Y\",\"pos\":[3,5]},{\"color\":\"Y\",\"pos\":[3,6]}]}}";
 		assertThrows(Exception.class, () -> GameTeamsBuilder.createGame(new JSONObject(inputJson)));
 	}
 	
 	@Test
 	void test_6() {
 		// if type is not GameClassic it returns null
-		String inputJson = "{\"teams\":[{\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"name\":\"equipo1\"},{\"players\":[{\"color\":\"R\",\"name\":\"dani\"}],\"name\":\"equipo2\"}],\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"turn\":\"BROWN\",\"type\":\"GameTeams\",\"board\":{\"shape\":\"SM\",\"cubes\":[{\"color\":\"W\",\"pos\":[3,4]},{\"color\":\"Y\",\"pos\":[kfnwie,5]},{\"color\":\"Y\",\"pos\":[3,6]}]}}";
+		String inputJson = "{\"teams\":[{\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"name\":\"equipo1\"},{\"players\":[{\"color\":\"R\",\"name\":\"dani\"}],\"name\":\"equipo2\"}],\"players\":[{\"color\":\"Y\",\"name\":\"Juandi\"},{\"color\":\"W\",\"name\":\"Leo\"}],\"turn\":\"W\",\"type\":\"GameTeams\",\"board\":{\"shape\":\"SM\",\"cubes\":[{\"color\":\"W\",\"pos\":[3,4]},{\"color\":\"Y\",\"pos\":[kfnwie,5]},{\"color\":\"Y\",\"pos\":[3,6]}]}}";
 		assertThrows(Exception.class, () -> GameTeamsBuilder.createGame(new JSONObject(inputJson)));
 	}
 }
