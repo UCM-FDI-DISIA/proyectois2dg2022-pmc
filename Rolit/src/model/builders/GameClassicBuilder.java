bf860893b0a5d37690cd98119a9a87fba73aad09
unknown <marram18@ucm.es>
2022-05-07 14:49:22 +0200

    javadoc de builder

--- Rolit/src/model/builders/GameClassicBuilder.java
+++ Rolit/src/model/builders/GameClassicBuilder.java
@@ -15,13 +15,24 @@ import model.logic.Player;
 import model.logic.Shape;
 import model.strategy.Strategy;
 
+/**
+ * clase que extiende a GameBuilder y que es encargada de cerar el tipo de juego clásico
+ * @author PMC
+ *
+ */
 public class GameClassicBuilder extends GameBuilder {
 	public static final String TYPE = "GameClassic";
 	
 	GameClassicBuilder(){
 		
 	}
-	
+	/**
+	 * Es el encargado de generar el juego de tipo clásico
+	 * Por medio de un JSONObject que contiene la información, asigna el turno al color correspondiente,
+	 * crea la lista de jugadores, así como los distintos jugadores, el tablero y los cubos
+	 * @param o JSONObject ue contiene la información del juego clásico que se quiere crear
+	 * @return devuelve el nuevo juego ya creado
+	 */
 	@Override
 	protected Game GenerateGame(JSONObject o) {
 		Color turn = Color.valueOfIgnoreCase(o.getString("turn").charAt(0));
@@ -48,11 +59,20 @@ public class GameClassicBuilder extends GameBuilder {
 		return new GameClassic(board, list_cubes, list_players, turn);
 	}
 
+	/**
+	 * comprueba que el parámetro type coincide con este tipo de juego (GameClassic)
+	 * @param type es una cadena de caracteres que indican un tipo de juego
+	 * @return devuelve true si type es GameClassic y false en caso contrario
+	 */
 	@Override
 	protected boolean match(String type) {
 		return TYPE.equals(type);
 	}
-
+	
+	/**
+	 * 
+	 * @return una cadena de caracteres con el tipo de juego GameClassic
+	 */
 	@Override
 	protected String getName() {
 		return TYPE;
